a1b1fb655c90dfc7dacea9b8275a8b294e1073ef
JuanPabloDiaz <juanchodis@hotmail.com>
2024-01-18 21:06:20 -0500

    Refactor Square component and add isWinning prop

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,31 @@ import Layout from "./Components/Layout";
 import PropTypes from "prop-types";
 import { GiTicTacToe } from "react-icons/gi";
 
-function Square({ value, onClick }) {
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return lines[i];
+    }
+  }
+  return null;
+}
+function Square({ value, onClick, isWinning }) {
   return (
     <button
-      className="square w-15 h-15 m-1 cursor-pointer rounded-xl bg-[#433633] text-center text-4xl font-bold leading-9 text-[#F7F0F5]"
+      className={`square w-15 h-15 m-1 cursor-pointer rounded-xl text-center text-4xl font-bold leading-9 ${
+        isWinning ? "bg-green-500" : "bg-[#433633]"
+      } text-[#F7F0F5]`}
       onClick={onClick}
     >
       {value}
@@ -17,6 +38,7 @@ function Square({ value, onClick }) {
 Square.propTypes = {
   value: PropTypes.string,
   onClick: PropTypes.func.isRequired,
+  isWinning: PropTypes.bool,
 };
 
 function Board() {
@@ -38,7 +60,7 @@ function Board() {
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
-    status = "Winner: " + winner;
+    status = "Winner: " + squares[winner[0]];
   } else {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
@@ -50,7 +72,12 @@ function Board() {
       </div>
       <div className="shadow-[#5C5552 m-2 grid grid-flow-col grid-rows-3 rounded-xl bg-[#DECBB7] p-4 shadow-xl">
         {[...Array(9)].map((_, i) => (
-          <Square key={i} value={squares[i]} onClick={() => handleClick(i)} />
+          <Square
+            key={i}
+            value={squares[i]}
+            onClick={() => handleClick(i)}
+            isWinning={winner?.includes(i)}
+          />
         ))}
       </div>
       <button
@@ -65,26 +92,6 @@ function Board() {
   );
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
 function App() {
   return (
     <>
@@ -93,7 +100,6 @@ function App() {
           <h1 className="text-3xl font-extrabold sm:text-3xl md:text-4xl lg:text-5xl">
             Tic Tac Toe
           </h1>
-          {/* <GiTicTacToe className="h-10 w-full" /> */}
         </div>
         <div className="gap-2 rounded-lg bg-[#8F857D] p-4 sm:gap-3 md:gap-4 lg:gap-6">
           <section className="">
